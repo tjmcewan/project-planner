30395db1ae171e40ac2d2e6d99560327ebf306ab
Tim McEwan <tim@mcewan.it>
2012-12-21 10:46:05 +1100

    defaultPlan & textarea

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,5 +2,29 @@
 
 planner.controller('PlannerController', function PlannerController($scope, planStorage) {
   var plan = $scope.plan = planStorage.get();
+
+  var defaultPlan = {
+    "name": "My Awesome Project",
+    "milestones": [{
+      "name": "Administrivia",
+      "tasks":[{
+        "name": "Proposal",
+        "hours": 20
+      },{
+        "name": "Project management",
+        "hours": 40
+      },{
+        "name": "Wrap-up",
+        "hours": 10
+      }]
+    },{
+      "name": "The rest",
+      "tasks": []
+    }]
+  };
+
+  // plan || plan = defaultPlan;
+  console.log(plan);
+
   window.pplan = plan;
 });

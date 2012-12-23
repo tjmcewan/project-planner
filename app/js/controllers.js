a17671aae8157c21594144d3b6c01e6c298dc511
Tim McEwan <tim@mcewan.it>
2012-12-23 15:54:42 +1100

    butons to save to localstorage & reset

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,30 +1,45 @@
 'use strict';
 
 planner.controller('PlannerController', function PlannerController($scope, planStorage) {
-  var plan = $scope.plan = planStorage.get();
+    var plan = planStorage.get();
 
-  var defaultPlan = {
-    "name": "My Awesome Project",
-    "milestones": [{
-      "name": "Administrivia",
-      "tasks":[{
-        "name": "Proposal",
-        "hours": 20
-      },{
-        "name": "Project management",
-        "hours": 40
-      },{
-        "name": "Wrap-up",
-        "hours": 10
-      }]
-    },{
-      "name": "The rest",
-      "tasks": []
-    }]
-  };
+    var defaultPlan = {
+        name: "My Awesome Project",
+        milestones: [{
+            name: "Administrivia",
+            tasks:[{
+                name: "Proposal",
+                hours: 20
+            },{
+                name: "Project management",
+                hours: 40
+            },{
+                name: "Wrap-up",
+                hours: 10
+            }]
+        },{
+            name: "The rest",
+            tasks: []
+        }]
+    };
 
-  // plan || plan = defaultPlan;
-  console.log(plan);
+    $scope.planSave = function() {
+        planStorage.put($scope.plan);
+    };
 
-  window.pplan = plan;
+    $scope.resetToDefault = function () {
+        $scope.plan = defaultPlan;
+    };
+
+    $scope.cleanSlate = function () {
+        $scope.plan = {};
+    };
+
+    // $scope.plan_as_string = JSON.stringify($scope.plan);
+
+    $scope.plan = plan || defaultPlan;
+
+    console.log(plan);
+    console.log(defaultPlan);
+
 });
--- app/js/services.js
+++ app/js/services.js
@@ -14,18 +14,18 @@ planner.factory( 'planStorage', function() {
   };
 });
 
-planner.factory('forEachLine', function forEachLine(textarea, func) {
-  var lines = textarea.value.replace(/\r\n/g, "\n").split("\n");
-  var newLines, i;
+// planner.factory('forEachLine', function forEachLine(textarea, func) {
+//   var lines = textarea.value.replace(/\r\n/g, "\n").split("\n");
+//   var newLines, i;
 
-  // Use the map() method of Array where available
-  if (typeof lines.map != "undefined") {
-    newLines = lines.map(func);
-  } else {
-    newLines = [];
-    i = lines.length;
-    while (i--) {
-      newLines[i] = func(lines[i]);
-    }
-  }
-});
+//   // Use the map() method of Array where available
+//   if (typeof lines.map != "undefined") {
+//     newLines = lines.map(func);
+//   } else {
+//     newLines = [];
+//     i = lines.length;
+//     while (i--) {
+//       newLines[i] = func(lines[i]);
+//     }
+//   }
+// });
